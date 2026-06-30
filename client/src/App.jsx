3e20c27b72e8ea91f95fec9ddfe17dda9754f0c7
HodManagmentSystem <harshubankhede@gmail.com>
2026-06-30 20:53:49 +0530

    feat: add UserDashboard component to the dashboard page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ContactUs from "./pages/ContactUs";
+import { Toaster } from "react-hot-toast";
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+      <Toaster />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
