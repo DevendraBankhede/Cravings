272f5e5345d2e3fd76297e4a1a33ce85b9b21e53
HodManagmentSystem <harshubankhede@gmail.com>
2026-08-22 15:20:14 +0530

    fix: pass error to next middleware in auth controller methods

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -55,7 +55,7 @@ export const RegisterUser = async (req, res, next) => {
     res.status(201).json({ message: "User Created Successfully" });
   } catch (error) {
     console.log(error.message);
-    next();
+    next(error);
   }
 };
 
@@ -91,7 +91,7 @@ export const LoginUser = async (req, res, next) => {
     });
   } catch (error) {
     console.log(error.message);
-    next();
+    next(error);
   }
 };
 
@@ -102,7 +102,7 @@ export const LogoutUser = async (req, res, next) => {
     res.status(200).json({ message: "Logout Sucessfully" });
   } catch (error) {
     console.log(error.message);
-    next();
+    next(error);
   }
 };
 
@@ -143,7 +143,7 @@ export const SendOtp = async (req, res, next) => {
     res.status(200).json({ message: `OTP sent on '${email}'` });
   } catch (error) {
     console.log(error.message);
-    next();
+    next(error);
   }
 };
 export const VerifyOtp = async (req, res, next) => {
@@ -159,14 +159,14 @@ export const VerifyOtp = async (req, res, next) => {
     const existingOTP = await OTP.findOne({ email });
     if (!existingOTP) {
       const error = new Error("OTP Expired");
-      const statusCode = 401;
+      error.statusCode = 401;
       return next(error);
     }
 
     const isVerified = await bcrypt.compare(otp, existingOTP.otp);
     if (!isVerified) {
       const error = new Error("OTP Expired");
-      const statusCode = 401;
+      error.statusCode = 401;
       return next(error);
     }
 
@@ -185,7 +185,7 @@ export const VerifyOtp = async (req, res, next) => {
       .json({ message: "OTP verified. Create You New Password Now" });
   } catch (error) {
     console.log(error.message);
-    next();
+    next(error);
   }
 };
 export const ResetPassword = async (req, res, next) => {
@@ -203,6 +203,6 @@ export const ResetPassword = async (req, res, next) => {
     res.status(200).json({ message: "Password Changed" });
   } catch (error) {
     console.log(error.message);
-    next();
+    next(error);
   }
 };
